dbcab58ada631407bcbc0904591c2ab5ec79d3fe
Staci Genesi <sgenesi@hotmail.com>
2021-02-07 11:34:49 -0700

    put, post, and delete routes for voters

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -39,4 +39,72 @@ router.get('/voter/:id', (req, res) => {
     });
 });
 
+// POST route to allow voter to register
+router.post('/voter', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.email];
+
+    db.run(sql, params, function (err, data) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'succes',
+            data: body,
+            id: this.lastID
+        });
+    });
+});
+
+//PUT route to allow user to update email address
+router.put('/voter/:id', (req, res) => {
+    // Data validation
+    const errors = inputCheck(req.body, 'email');
+    if (errors) {
+        res.status(400).json({ errors: errors });
+        return;
+    }
+
+    // Prepare statement
+    const sql = `UPDATE voters SET email = ? WHERE id = ?`;
+    const params = [req.body.email, req.params.id];
+
+    // Execute
+    db.run(sql, params, function (err, data) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'succes',
+            data: req.body,
+            changes: this.changes
+        });
+    });
+});
+
+// DELETE route to remove voters from db
+router.delete('/voter/:id', (req, res) => {
+    const sql = `DELETE FROM voters WHERE id = ?`;
+
+    db.run(sql, req.params.id, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+
+        res.json({ message: 'deleted', changes: this.changes });
+    });
+});
+
 module.exports = router;
